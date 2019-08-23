d97a4ebf7ad0a3001ed888d1c9be5534874412d2
SamKahnke <samkahnke@gmail.com>
2019-08-23 14:36:08 -0500

    remove sessionID

--- login-test/login-test.js
+++ login-test/login-test.js
@@ -1,9 +1,12 @@
 const PHPBB_ROOT = '../phpbb/';
-var SessionID = 'dffffc0c8c002bf14e1da49fcdb1c49d';
+// var SessionID = 'dffffc0c8c002bf14e1da49fcdb1c49d';
 var UserName = 'jsargent';
 var Password = 'jsargent';
-var LoginURL = PHPBB_ROOT + 'ucp.php?mode=login&amp;sid='+SessionID;
-var TopicURL = PHPBB_ROOT + 'viewtopic.php?f=4&t=3&sid='+SessionID;
+// var LoginURL = PHPBB_ROOT + 'ucp.php?mode=login&amp;sid='+SessionID;
+// var TopicURL = PHPBB_ROOT + 'viewtopic.php?f=4&t=3&sid='+SessionID;
+var LoginURL = PHPBB_ROOT + 'ucp.php?mode=login';
+var TopicURL = PHPBB_ROOT + 'viewtopic.php?f=4&t=3';
+
 
 function FormSetup(){
     var a = document.getElementById("form1");
