e6cbed1939b325dcb78e7423ab891ebe5bb4b8d6
SamKahnke <samkahnke@gmail.com>
2019-09-01 17:52:31 -0500

    set up a basic test router

--- phpbb/LCMS_api/function-test.php
+++ phpbb/LCMS_api/function-test.php
@@ -3,21 +3,18 @@ define('IN_PHPBB', true);
 $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '../';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
-include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
 
+function get_forum_names()
+{
+	global $db;
 
-
-	function get_forum_names()
+	$sql = 'SELECT *
+	FROM ' . FORUMS_TABLE;
+	$result = $db->sql_query($sql);
+	while ($row = $db->sql_fetchrow($result))
 	{
-		global $db;
-
-		$sql = 'SELECT *
-		FROM ' . FORUMS_TABLE;
-		$result = $db->sql_query($sql);
-		while ($row = $db->sql_fetchrow($result))
-		{
-			echo $row["forum_name"] . "<br>";
-		}
+		echo $row["forum_name"] . "<br>";
 	}
+}
 
-	get_forum_names();
+get_forum_names();
--- phpbb/LCMS_api/hello.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<p>Name " . $_GET["puppies"] . "</p>";
-// var $puppies = request_var('puppies', 0);
