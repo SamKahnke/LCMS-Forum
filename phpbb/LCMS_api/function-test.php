2b44019d032372c92ea818bdd1636df9baa88f56
SamKahnke <samkahnke@gmail.com>
2019-09-01 12:11:12 -0500

    access phpbb functions in function-test

--- phpbb/LCMS_api/function-test.php
+++ phpbb/LCMS_api/function-test.php
@@ -1,6 +1,6 @@
 <?php
 define('IN_PHPBB', true);
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '../';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
 include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
@@ -10,9 +10,14 @@ global $db;
 	// Here we check permission consistency
 
 	// Sometimes, it can happen permission tables having forums listed which do not exist
-	$sql = 'SELECT forum_id
+	$sql = 'SELECT *
 		FROM ' . FORUMS_TABLE;
 	$result = $db->sql_query($sql);
 
+	echo $sql;
+	echo "<br>";
 	echo $result;
+	while ($row = $db->sql_fetchrow($result)) {
+		echo $row["forum_name"];
+	}
 // echo get_forum_list();
